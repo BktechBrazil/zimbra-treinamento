afb330da8a6b9ff8b9402b0ad75fff237946884b
Fabio Soares Schmidt <fabio@improve.inf.br>
2020-06-30 21:28:37 -0300

    Update editar_hosts.sh

--- editar_hosts.sh
+++ editar_hosts.sh
@@ -6,5 +6,5 @@ ONLYHOST=`hostname -f | cut -d . -f 1`
 
 LOCALHOST="127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4"
 
-echo  "$LOCALHOST"
-echo "$IPADDRESS $HOSTNAME $ONLYHOST"
+echo  "$LOCALHOST" > /etc/hosts
+echo "$IPADDRESS $HOSTNAME $ONLYHOST" >> /etc/hosts
