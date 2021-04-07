feaa35da3c5be7d3d93fc4889d06fcc941ce4933
Fabio Soares Schmidt <fabio@improve.inf.br>
2021-04-07 14:06:05 -0300

    Update create_dns_A_record.sh

--- azure/create_dns_A_record.sh
+++ azure/create_dns_A_record.sh
@@ -3,12 +3,13 @@
 IFS=$'\n'
 
 RESOURCEGROUP="zimbra-training-2021"
+HOST="mail"
 
 for DOMINIOS in `cat dominios.txt`
 do
   DOMINIO=`echo "$DOMINIOS" | cut -d "," -f 10`
   IP=`echo "$DOMINIOS" | cut -d "," -f 8`
 
-          echo "az network dns record-set a add-record --resource-group $RESOURCEGROUP --zone-name $DOMINIO --record-set-name mail --ipv4-address $IP"
+          echo "az network dns record-set a add-record --resource-group $RESOURCEGROUP --zone-name $DOMINIO --record-set-name $HOST --ipv4-address $IP"
 
 done
