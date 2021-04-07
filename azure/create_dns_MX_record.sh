d08c3cf5f24d8adfca8131f6a950f76622168195
Fabio Soares Schmidt <fabio@improve.inf.br>
2021-04-07 10:41:13 -0300

    Update create_dns_MX_record.sh

--- azure/create_dns_MX_record.sh
+++ azure/create_dns_MX_record.sh
@@ -7,8 +7,7 @@ RESOURCEGROUP="zimbra-training-2021"
 for DOMINIOS in `cat dominios.txt`
 do
   DOMINIO=`echo "$DOMINIOS" | cut -d "," -f 10`
-  IP=`echo "$DOMINIOS" | cut -d "," -f 8`
-
-          echo "az network dns record-set mx add-record --resource-group $RESOURCEGROUP --zone-name $DOMINIO --record-set-name \"@\" --exchange mail.$DOMINIO --preference 0"
+  
+  echo "az network dns record-set mx add-record --resource-group $RESOURCEGROUP --zone-name $DOMINIO --record-set-name \"@\" --exchange mail.$DOMINIO --preference 0"
 
 done
