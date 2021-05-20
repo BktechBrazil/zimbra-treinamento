3485912195c293d33228889af8c141e1128c310e
Fabio Soares Schmidt <fabio@improve.inf.br>
2021-05-20 15:49:13 -0300

    Update centos8_ambiente-basico.sh

--- centos8_ambiente-basico.sh
+++ centos8_ambiente-basico.sh
@@ -2,3 +2,4 @@ dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.r
 dnf install tar dnsmasq perl nmap-ncat bind-utils net-tools screen telnet vim screen htop wget rsyslog -y
 dnf update -y
 sed -i s/^SELINUX=.*$/SELINUX=disabled/ /etc/selinux/config
+setenforce 0
