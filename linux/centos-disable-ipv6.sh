84f009f8ee6607376220ed66f81b23ea44f48f20
Fabio Soares Schmidt <fabio@improve.inf.br>
2021-08-23 19:21:18 -0300

    Create centos-disable-ipv6.sh

---
+++ linux/centos-disable-ipv6.sh
@@ -0,0 +1,4 @@
+echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
+echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
+echo "net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf
+sysctl -p
