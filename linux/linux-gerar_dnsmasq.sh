20d4b5cce639d1d48fc1ad4c630eb0acc83aa728
Fabio Soares Schmidt <fabio@improve.inf.br>
2022-04-26 19:39:52 -0300

    Update linux-gerar_dnsmasq.sh

--- linux/linux-gerar_dnsmasq.sh
+++ linux/linux-gerar_dnsmasq.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IPADDRESS="`ip -4 addr show ens18 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'`"
+IPADDRESS="`ip -4 addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'`"
 HOSTNAME="`hostname -f`"
 DOMAIN=`hostname -f | cut -d . -f 2,3,4,5`
 
