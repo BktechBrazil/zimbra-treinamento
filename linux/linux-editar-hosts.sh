c799294425a8e06ecd7206320bda620546dba66f
Fabio Soares Schmidt <fabio@improve.inf.br>
2023-08-22 20:24:46 -0300

    Update linux-editar-hosts.sh

--- linux/linux-editar-hosts.sh
+++ linux/linux-editar-hosts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IPADDRESS="`ip -4 addr show eth1 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'`"
+IPADDRESS="`ip -4 addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'`"
 HOSTNAME="`hostname -f`"
 ONLYHOST=`hostname -f | cut -d . -f 1`
 
