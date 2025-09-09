33a99cc66a4636f0d6b107529f98fedaa82924ae
Fabio Soares Schmidt <fabio@improve.inf.br>
2025-09-09 17:29:12 -0300

    Create centos9_ambiente-basico.sh

---
+++ linux/centos_rhel/centos9_ambiente-basico.sh
@@ -0,0 +1,6 @@
+dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm -y
+dnf install tar dnsmasq perl nmap-ncat bind-utils net-tools screen telnet vim screen htop wget rsyslog -y
+dnf update -y
+sed -i s/^SELINUX=.*$/SELINUX=disabled/ /etc/selinux/config
+setenforce 0
+systemctl disable --now firewalld
